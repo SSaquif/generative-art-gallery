afa15d796409eafea123291eb4f2164ff18d7781
Sadnan Saquif <ssaquif@gmail.com>
2020-09-09 18:58:00 -0400

    Updates

---
+++ precache-manifest.acf5b51be3b646205e360816292b844d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4fd622635a44987d4d9be74fcddd87",
+    "url": "/gen-art/index.html"
+  },
+  {
+    "revision": "f015b9065fb548b4bcfb",
+    "url": "/gen-art/static/js/2.94aedf66.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/gen-art/static/js/2.94aedf66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe34730a5a064af1ee7",
+    "url": "/gen-art/static/js/main.5c2e51c8.chunk.js"
+  },
+  {
+    "revision": "92978de8a742ae0dccdd",
+    "url": "/gen-art/static/js/runtime-main.7f5577b0.js"
+  }
+]);
