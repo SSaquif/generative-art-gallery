876e7eabb66c221ecd2b5af5316a4a175bad6124
Sadnan Saquif <ssaquif@gmail.com>
2020-09-10 05:07:34 -0400

    Added navigation functionaly, so can shocase for than one sketch at a time

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Sketch from "./Sketch";
 import styled from "styled-components";
+import artDirectory from "../Art-Directory/artDirectory";
 
 const App = () => {
+  const [sketch, setSketch] = useState(artDirectory[0]);
+  const [sketchIndex, setSketchIndex] = useState(0);
+
+  const lt = "<";
+  const gt = ">";
+
+  const handlePrev = (ev) => {
+    if (sketchIndex) {
+      setSketch(artDirectory[sketchIndex - 1]);
+      setSketchIndex(sketchIndex - 1);
+    }
+  };
+
+  const handleNext = (ev) => {
+    if (sketchIndex < artDirectory.length - 1) {
+      setSketch(artDirectory[sketchIndex + 1]);
+      setSketchIndex(sketchIndex + 1);
+    }
+  };
+
   return (
     <>
       <SketchContainer>
-        <Sketch />
+        <PrevButton onClick={handlePrev}> {lt} </PrevButton>
+        <Sketch sketch={sketch} />
+        <NextButton onClick={handleNext}> {gt} </NextButton>
       </SketchContainer>
     </>
   );
@@ -19,4 +42,33 @@ const SketchContainer = styled.div`
   height: 100%;
 `;
 
+const PrevButton = styled.button`
+  display: inline-block;
+  border: none;
+  background-color: black;
+  color: white;
+  font-size: 5em;
+  opacity: 0.3;
+  padding: 1.5%;
+  margin-right: 2%;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const NextButton = styled.button`
+  display: inline-block;
+  border: none;
+  background-color: black;
+  color: white;
+  font-size: 5em;
+  opacity: 0.3;
+  padding: 1.5%;
+  margin-left: 2%;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
 export default App;
--- src/Components/Sketch.js
+++ src/Components/Sketch.js
@@ -6,57 +6,9 @@ const settings = {
   dimensions: [2048, 2048],
 };
 
-const createGrid = () => {
-  const points = [];
-  const count = 5;
-  for (let x = 0; x < count; x++) {
-    for (let y = 0; y < count; y++) {
-      // Switching to a [u,v] co-ordiante system
-      const u = x / count;
-      const v = x / count;
-      points.push([u, v]);
-    }
-  }
-  return points;
-};
-
-const sketch = (sketchArgs) => {
-  const points = createGrid();
-  console.log(points);
-
-  return ({ context, width, height }) => {
-    context.fillStyle = "#544813";
-    context.fillRect(0, 0, width, height);
-
-    points.forEach(([u, v]) => {
-      //creating the circles
-      const x = u * width;
-      const y = v * height;
-
-      context.beginPath();
-      context.arc(x, y, width / 5, 0, 2 * Math.PI, false);
-      context.strokeStyle = "red";
-      context.lineWidth = 30;
-      context.stroke();
-
-      context.beginPath();
-      context.arc(x * 1.5, y, width / 5, 0, 2 * Math.PI, false);
-      context.strokeStyle = "crimson";
-      context.lineWidth = 30;
-      context.stroke();
-
-      context.beginPath();
-      context.arc(x * 0.5, y, width / 5, 0, 2 * Math.PI, false);
-      context.strokeStyle = "orange";
-      context.lineWidth = 30;
-      context.stroke();
-    });
-  };
-};
-
-const Sketch = () => {
-  //   const ref = createRef();
-  const ref = useRef();
+const Sketch = ({ sketch }) => {
+  const ref = createRef();
+  // const ref = useRef();
 
   useEffect(() => {
     settings.canvas = ref.current;
