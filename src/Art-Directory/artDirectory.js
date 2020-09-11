d7ec8646e03f33da5003adbabb4eb5bff5bb845c
Sadnan Saquif <ssaquif@gmail.com>
2020-09-11 03:28:07 -0400

    Added module canvas-sketch-util Another sketch added

--- src/Art-Directory/artDirectory.js
+++ src/Art-Directory/artDirectory.js
@@ -1,6 +1,7 @@
 import circleBeamSketch from "../Art/circleBeam";
 import circlesSketch from "../Art/circles";
+import nightskySketch from "../Art/nightsky";
 
-const artDirectory = [circleBeamSketch, circlesSketch];
+const artDirectory = [circleBeamSketch, circlesSketch, nightskySketch];
 
 export default artDirectory;
---
+++ src/Art/nightsky.js
@@ -0,0 +1,42 @@
+const { lerp } = require("canvas-sketch-util/math");
+
+const createGrid = () => {
+  const points = [];
+  const count = 150;
+  for (let x = 0; x < count; x++) {
+    for (let y = 0; y < count; y++) {
+      // Switching to a [u,v] co-ordiante system
+      const u = count <= 1 ? 0.5 : x / (count - 1);
+      const v = count <= 1 ? 0.5 : y / (count - 1);
+      points.push([u, v]);
+    }
+  }
+  return points;
+};
+
+const nightskySketch = () => {
+  const margin = 125;
+  const filteredPoints = createGrid().filter(([u, v]) => {
+    return Math.random() > 0.98;
+  });
+  console.log(filteredPoints);
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "#170b36";
+    context.fillRect(0, 0, width, height);
+
+    filteredPoints.forEach(([u, v]) => {
+      //creating the circles
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, 0.063, 0, 2 * Math.PI, false);
+      context.strokeStyle = "white";
+      context.lineWidth = 40;
+      context.stroke();
+    });
+  };
+};
+
+export default nightskySketch;
