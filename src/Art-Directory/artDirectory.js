8fe6af57fa842db2eb56034c49409a5dc620c293
Sadnan Saquif <ssaquif@gmail.com>
2020-09-15 18:30:14 -0400

    added sketch: whitePink

--- src/Art-Directory/artDirectory.js
+++ src/Art-Directory/artDirectory.js
@@ -2,12 +2,14 @@ import circleBeamSketch from "../Art/circleBeam";
 import circlesSketch from "../Art/circles";
 import nightskySketch from "../Art/nightsky";
 import debrisSketch from "../Art/debris";
+import whitePinkSketch from "../Art/whitePink";
 
 const artDirectory = [
   circleBeamSketch,
   circlesSketch,
   nightskySketch,
   debrisSketch,
+  whitePinkSketch,
 ];
 
 export default artDirectory;
